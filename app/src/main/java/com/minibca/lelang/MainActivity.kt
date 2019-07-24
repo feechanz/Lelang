2cbd58f499696e764812bb4ade99b2d7f823c420
Chandra <feechanz@yahoo.co.id>
2019-07-24 22:20:19 +0700

    Initial project

---
+++ app/src/main/java/com/minibca/lelang/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.minibca.lelang
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
